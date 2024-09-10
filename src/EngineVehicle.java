0e54c7f2bb713d467f15c0a4e60da27117df599b
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2024-09-10 23:11:39 +0300

    MODIFICATION Homework3 done.

---
+++ src/EngineVehicle.java
@@ -0,0 +1,4 @@
+public interface EngineVehicle extends Vehicle {//расширяем интерфейс для того чтобы он мог тоже
+    // применить метод по замене покрышки
+    void checkEngine(); // Проверка двигателя
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,5 @@
+public interface Vehicle {
+
+        void updateTyre(); // Замена покрышек
+    }
+
